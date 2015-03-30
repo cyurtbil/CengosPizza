b32919fa5ce33fbbbf9febac4ded69be951d8424
cyurtbil <cenky92@gmail.com>
2015-03-30 13:18:56 -0400

    added grunt to the project

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+// Initial configuration
+// ========================================================
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      server: { // task name
+        options: {
+          port: '8000',
+          protocol: 'http',
+          hostname: 'localhost',
+          base: 'app',
+          keepalive: true,
+          livereload: true,
+          open: true
+        }
+      }
+    },
+    wiredep: {
+      task: {
+        src: ['app/index.html'],
+      }
+    }
+  });
+
+// Loading npm tasks
+// ========================================================
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-wiredep');
+// ========================================================
+
+// Register npm tasks
+// ========================================================
+  grunt.registerTask('default', ['connect', 'wiredep']);
+// ======================================================== 
+};
