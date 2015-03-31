50aadb10f4d60520bb8f2d937c1f34ca0620edd8
cyurtbil <cenky92@gmail.com>
2015-03-31 11:37:52 -0400

    added some useer experience flow

--- app/js/lib/eventHandlers.js
+++ app/js/lib/eventHandlers.js
@@ -2,6 +2,8 @@ var selectCityEvent = function() {
   $('ul#cities li').on('click', function(event) {
     $(event.target).addClass('hovered');
     loadPizzas(event.target.innerHTML, event);
+    $('section h2').hide();
+    $('#pizza-container').show();
   });
 };
 
